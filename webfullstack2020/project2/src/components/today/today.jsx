29660995456436b6d6ca86c2f4c61637940a6440
jessiehan.au@gmail.com <jessiehan.au@gmail.com>
2020-05-16 19:47:42 +1000

    change today class to function

--- webfullstack2020/project2/src/components/today/today.jsx
+++ webfullstack2020/project2/src/components/today/today.jsx
@@ -1,60 +1,47 @@
 import React from "react";
 import styles from "./style.module.css";
+import "../weather.css";
 
-class Today extends React.Component{
-  constructor(props){
-    super(props);
-    this.state={};
-    // this.apiCall();
-  }
+function Today({data}){
+   if(!data){
+    console.log("today data undefined ");
+    return(
+      <div className={styles.container}>
+        <div className="loadding"></div>
+      </div>
+    );
+   }
 
-  async apiCall(url){
-  //  console.log("api fetch: "+url);
-    const response = await fetch(url);
-    const data = await response.json();
-    // console.log("api call: "+ data.main.temp);
-    const currentDate = new Date();
-    this.setState({
-      "temp": Math.round(data.main.temp),
-      "tempMin": Math.round(data.main.temp_min),
-      "tempMax": Math.round(data.main.temp_max),
-      "city": data.name,
-      "weather": data.weather[0].main,
-      "date": currentDate.toDateString()
-    });
+  const currentDate = new Date();
+  const weather={
+    "temp": Math.round(data.main.temp),
+    "tempMin": Math.round(data.main.temp_min),
+    "tempMax": Math.round(data.main.temp_max),
+    "city": data.name,
+    "weather": data.weather[0].main,
+    "date": currentDate.toDateString()
+  };
 
-    console.log(data.weather[0].main);
-  }
-  componentDidMount(){
-    this.apiCall(this.props.url);
-  }
-  
-  componentWillReceiveProps(nextProps, prevState){
-    this.apiCall(nextProps.url);
-  }
-  render(){
   //  console.log("this.props.url"+this.props.url);
     return(
       <div className={styles.container}>
         <div className={styles.cityname}>
-          {this.state.city}
+          {weather.city}
         </div>
         <div className={styles.weather}>
-          {this.state.weather}
+          {weather.weather}
         </div>
         <div className={styles.temperature}>
-            {this.state.temp} ℃
+            {weather.temp} ℃
         </div>
         <div className={styles.minmax}>
-            {this.state.tempMin}℃ ~ {this.state.tempMax}℃
+            {weather.tempMin}℃ ~ {weather.tempMax}℃
         </div>
         <div className={styles.date}>
-          {this.state.date}
+          {weather.date}
         </div>
       </div>
     );
-}
-
 }
 
 export default Today;
