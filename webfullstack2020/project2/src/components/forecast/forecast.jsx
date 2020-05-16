4a2b194749a1f1539c5316835205eb15f99088bd
jessiehan.au@gmail.com <jessiehan.au@gmail.com>
2020-05-16 19:48:55 +1000

    add loading animation

--- webfullstack2020/project2/src/components/forecast/forecast.jsx
+++ webfullstack2020/project2/src/components/forecast/forecast.jsx
@@ -1,71 +1,79 @@
 import React from "react";
 import styles from "./style.module.css";
+import "../weather.css";
+import clear from "../../images/icons/clear.svg";
+import rain from "../../images/icons/rain.svg";
+import cloudy from "../../images/icons/cloudy.svg";
 
 const week = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+ const weatherPic = {
+   "Clear":clear,
+   "Rain":rain,
+   "Clouds":cloudy
+ };
+
 function Day({data,key}){
 //  console.log(data);
+  if(!data){
+    return(
+      <div className={styles.day}>
+      <div className="loadding"></div>
+      </div>
+    );
+  }
+
+  const src = weatherPic[data.weather];
   const date = new Date(data.seconds*1000);
   return(
     <div className={styles.day}>
       <div>{week[date.getDay()]}</div>
       <div>{data.temp}℃</div>
       <div>{data.weather}</div>
+      <img src={src} alt="clear"></img>
     </div>
   );
 }
 
-class Forecast extends React.Component{
-  constructor(props){
-    super(props);
-    this.state={
-      forecast:[]
-    };
-  }
-
-  async apiCall(url){
-    console.log("api fetch forecast: "+url);
-    const response = await fetch(url);
-    const data = await response.json();
-    const count = data.cnt;
-    const forecast = [];
-    for(let i = 7; i < count; i += 8){
-      console.log(data.list[i]);
-      const current = data.list[i];
-      const currentWeather={
-              "temp": Math.round(current.main.temp),
-              "tempMin": current.main.temp_min,
-              "tempMax":current.main.temp_max,
-              "weather": current.weather[0].main,
-              "seconds": current.dt
-             };
-      forecast.push(currentWeather);
-      console.log(currentWeather);
-    }
-    this.setState({"forecast":forecast});
-    }
-
-  componentDidMount(){
-    this.apiCall(this.props.url);
-    console.log("forecast loading");
-  }
-
-  componentWillReceiveProps(nextProps, prevState){
-    this.apiCall(nextProps.url);
-  }
-
-  render(){
+function Forecast({data}){
+//  console.log("forecast: " + data);
+  if(!data){
+    console.log("forecast data undefined ");
     return(
-      <div className={styles.container}>        
-        {
-          this.state.forecast.map((item,index) => {
-            return(
-              <Day data={item} key={index}></Day>
-            );})
-          }
+      <div className={styles.container}>  
+      <Day></Day>
+      <Day></Day>
+      <Day></Day>
+      <Day></Day>
+      <Day></Day>
       </div>
     );
-    
-    }
   }
+
+  const forecast = [];
+  for(let i = 7; i < data.cnt; i += 8){
+    const current = data.list[i];
+    const currentWeather={
+            "temp": Math.round(current.main.temp),
+            "tempMin": current.main.temp_min,
+            "tempMax": current.main.temp_max,
+            "weather": current.weather[0].main,
+            "seconds": current.dt
+            };
+    forecast.push(currentWeather);
+//    console.log(currentWeather);
+  }
+
+  return(
+    <div className={styles.container}>        
+      {
+        forecast.map((item,index) => {
+          return(
+            <Day data={item} key={index}></Day>
+          );})
+        }
+    </div>
+  );
+}
+
 
 export default Forecast;
